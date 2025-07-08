b2ff11e6edcc79842b508b88262e6f24c4c2430f
0XAymax <aymanderrouich3@gmail.com>
2025-07-08 04:39:01 -0700

    updating admin layout to use side bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,43 +1,39 @@
 "use client";
-import Logo from "@/components/Logo";
 import AdminPage from "@/components/AdminPage";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/modeToggle";
-import { signOut } from "next-auth/react";
-import Link from "next/link";
 import React from "react";
+import { Globe, Inbox, LogOut } from "lucide-react";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import DashboardHeader from "@/components/shared/DashboardHeader";
 function AdminLayout({ children }: { children: React.ReactNode }) {
+  const adminItems =[
+    {
+      title: "Conference Requests",
+      url: "/admin/conference-requests",
+      icon: Globe,
+    },
+    {
+      title: "Inbox",
+      url: "/admin/inbox",
+      icon: Inbox,
+    },
+  ]
+  const secondaryItems = [
+  {
+    title: "Logout",
+    url: "/dashboard/sign-out",
+    icon: LogOut,
+  },
+]
   return (
     <AdminPage>
-      <header className="flex justify-between items-center h-16 py-3 px-5.5 border-border border-b-1">
-        <Link href="/admin">
-          <Logo height={120} width={110} /> Admin
-        </Link>
-        <nav className="flex items-center gap-8">
-          <Link
-            href="/admin/conference-requests"
-            className="text-[#64748b] hover:text-[#0f172a] transition-colors font-medium"
-          >
-            Conference Requests
-          </Link>
-          <Link
-            href="/admin/Inbox"
-            className="text-[#64748b] hover:text-[#0f172a] transition-colors font-medium"
-          >
-            Inbox
-          </Link>
-        </nav>
-        <div className="flex gap-4">
-          <Button
-            className="cursor-pointer"
-            onClick={() => signOut({ callbackUrl: "/sign-in" })}
-          >
-            Sign Out
-          </Button>
-          <ModeToggle />
-        </div>
-      </header>
-      {children}
+      <SidebarProvider>
+        <AppSidebar items={adminItems} secondaryItems={secondaryItems} />
+        <SidebarInset>
+          <DashboardHeader />
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
     </AdminPage>
   );
 }
