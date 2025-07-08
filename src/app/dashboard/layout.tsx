ff59389c45f6e6488d5fd5ddde1003ff98b3097a
0XAymax <aymanderrouich3@gmail.com>
2025-07-08 04:39:48 -0700

    updating dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,59 @@
 "use client";
-import { AppSidebar } from '@/components/app-sidebar';
-import ProtectedPage from '@/components/ProtectedPage';
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import React from 'react'
-import DashboardHeader from './DashboardHeader';
-function DashboardLayout({children}: {children: React.ReactNode}) {
+import { AppSidebar } from "@/components/app-sidebar";
+import ProtectedPage from "@/components/ProtectedPage";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import React from "react";
+import DashboardHeader from "../../components/shared/DashboardHeader";
+import {
+  Compass,
+  Globe,
+  Inbox,
+  LogOut,
+  ShieldCheck,
+  UserCircle,
+} from "lucide-react";
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const userItems = [
+    {
+      title: "All Conferences",
+      url: "/dashboard/all-conferences",
+      icon: Globe,
+    },
+    {
+      title: "My Conferences",
+      url: "/dashboard/my-conferences",
+      icon: Compass,
+    },
+    {
+      title: "My Roles",
+      url: "/dashboard/my-roles",
+      icon: ShieldCheck,
+    },
+    {
+      title: "Inbox",
+      url: "/dashboard/inbox",
+      icon: Inbox,
+    },
+  ];
+  const secondaryItems = [
+    {
+      title: "My Account",
+      url: "/dashboard/account",
+      icon: UserCircle,
+    },
+    {
+      title: "Logout",
+      url: "/dashboard/sign-out",
+      icon: LogOut,
+    },
+  ];
   return (
     <ProtectedPage>
       <SidebarProvider>
-        <AppSidebar />
+        <AppSidebar items={userItems} secondaryItems={secondaryItems} />
         <SidebarInset>
           <DashboardHeader />
           {children}
